387afccd0f4638008629db3cd7b53327ef1dfc53
Tommy <meier@rhinos-media.de>
2016-03-11 16:05:17 +0100

    more testing

--- test.php
+++ test.php
@@ -6,6 +6,8 @@
  * Time: 11:25
  */
 
+use WCKZ\Console\Console as Console;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $authentication = WCKZ\Afterbuy\Authentication::fromYAML('config.yaml');
@@ -17,3 +19,31 @@ $request = $afterbuy->createRequest('GetShopProducts', array(
     'pageNumber'        => 1
 ));
 
+$request->send();
+$productCount = (int) $request->getResponse()['Result']['PaginationResult']['TotalNumberOfEntries'];
+$pageNumber   = 1;
+$products     = array();
+
+Console::writeLine('Found %d products.', $productCount);
+$request->update('maxShopItems', 100);
+
+while($productCount > 0)
+{
+    Console::writeLine('Requesting products...');
+    $request->update('pageNumber', ++$pageNumber);
+    $request->send();
+
+    if($request->hasError())
+    {
+        Console::writeLine('Request failed... aborting');
+        Console::stop();
+    }
+
+    $newProducts   = $request->getResponse()['Result']['Products']['Product'];
+    $products      = array_merge($newProducts, $products);
+    $productCount -= count($newProducts);
+
+    Console::writeLine('Got %d products. (%d left)', count($newProducts), $productCount);
+}
+
+Console::writeLine('Fetched %d products.', count($products));
